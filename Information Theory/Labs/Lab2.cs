609f5c0d904216f1a0fb826f2dc9f675712a3705
oshipka <olenkashipka@gmail.com>
2020-03-10 02:35:25 +0200

    lab 2 done

--- Information Theory/Labs/Lab2.cs
+++ Information Theory/Labs/Lab2.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Runtime.Remoting.Metadata.W3cXsd2001;
 
@@ -12,6 +13,7 @@ namespace Information_Theory.Labs
 			new[] {0.016, 0.3480, 0.036},
 			new[] {0.0315, 0.0140, 0.3045}
 		};
+
 		/*private static double[][] matrix_t1 =
 		{
 			new[] {0.1275, 0.009, 0.0135},
@@ -31,22 +33,23 @@ namespace Information_Theory.Labs
 			new[] {0.81, 0.19},
 			new[] {0.05, 0.95}
 		};
+
 		private static int v0_t3 = 1000;
-		
+
 		public static void Task1()
 		{
 			Console.WriteLine("I(X;Y)");
 			var I = Lab2.I(matrix_t1);
 			Console.WriteLine("V");
-			var v = Lab2.V(v0_t1, I );
+			var v = Lab2.V(v0_t1, I);
 			Console.WriteLine("P(X|Y)");
 			var pXifY = Lab1.Pxify(matrix_t1, Lab1.Py(matrix_t1));
 			Console.WriteLine("P(Y|X)");
 			var pYifX = Lab1.PYifX(matrix_t1);
-            Console.WriteLine("C");
+			Console.WriteLine("C");
 			var c = Lab2.C(v0_t1, pYifX);
 			string[] val = {"I(X;Y)", "V", "C"};
-			AnsverTable(val, new []{I, v, c});
+			AnsverTable(val, new[] {I, v, c});
 		}
 
 		public static void Task2()
@@ -57,13 +60,14 @@ namespace Information_Theory.Labs
 
 		public static void Task3()
 		{
-
+			C_num(matrix_t3, v0_t3, 0.001);
 		}
+
 		private static double I(double[][] matrix_pxandy)
 		{
-			var px  = Lab1.Px(matrix_pxandy);
+			var px = Lab1.Px(matrix_pxandy);
 			Lab1.H(px, out var Hx);
-			var py  = Lab1.Py(matrix_pxandy);
+			var py = Lab1.Py(matrix_pxandy);
 			Lab1.H(py, out var Hy);
 			var hXandY = Lab1.HXandY(matrix_pxandy);
 			var res = Hx + Hy - hXandY;
@@ -88,9 +92,9 @@ namespace Information_Theory.Labs
 
 			res += max;
 			res *= v0;
-			Console.WriteLine("C =\n "+v0 + "*("+precalc+")=\n "+v0 + "*("+calc+")="+Math.Round(res, 3));
+			Console.WriteLine("C =\n " + v0 + "*(" + precalc + ")=\n " + v0 + "*(" + calc + ")=" + Math.Round(res, 3));
 			return res;
-			
+
 		}
 
 		private static double C(double p, double q, double pb, int v0)
@@ -119,39 +123,129 @@ namespace Information_Theory.Labs
 			v = q * Math.Log(q, 2);
 			res_str += v + " + ";
 			res += v;
-			v = (1-pb) * (1-Math.Log((1-pb), 2));
+			v = (1 - pb) * (1 - Math.Log((1 - pb), 2));
 			res_str += v + ") = \n";
 			res += v;
 			res *= v0;
-			Console.WriteLine("C =\n "+res_str+")="+Math.Round(res, 3));
+			Console.WriteLine("C =\n " + res_str + ")=" + Math.Round(res, 3));
 			return res;
 		}
+
+		private static void C_num(double[][] matrix_pYifX, int v0, double step = 0.001)
+		{
+			var px = 0.001;
+			var HYx0 = matrix_pYifX[0][0] * Math.Log(matrix_pYifX[0][0], 2) +
+			           matrix_pYifX[0][1] * Math.Log(matrix_pYifX[0][1], 2);
+			var HYx0_string = matrix_pYifX[0][0] +
+			                  " * log2(" +
+			                  matrix_pYifX[0][0] +
+			                  ") + " +
+			                  matrix_pYifX[0][1] +
+			                  " * log2(" +
+			                  matrix_pYifX[0][1] +
+			                  ")";
+			var HYx1 = matrix_pYifX[1][0] * Math.Log(matrix_pYifX[1][0], 2) +
+			           matrix_pYifX[1][1] * Math.Log(matrix_pYifX[1][1], 2);
+			var HYx1_string = matrix_pYifX[1][0] +
+			                  " * log2(" +
+			                  matrix_pYifX[1][0] +
+			                  ")" +
+			                  matrix_pYifX[1][1] +
+			                  " * log2(" +
+			                  matrix_pYifX[1][1] +
+			                  ")";
+			var max = double.MinValue;
+			var res_str_preprecalc = "";
+			var res_str_precalc = "";
+			var res_str_calc = "";
+			while (px <= 1)
+			{
+				var py = 0.76 * px + 0.05;
+				var Hy = -1 * (py * Math.Log(py, 2) + (1 - py) * Math.Log(1 - py, 2));
+				var HYifX = -1 * (px * HYx0 + (1 - px) * HYx1);
+
+				var new_max = Hy - HYifX;
+
+				if (new_max > max)
+				{
+					max = new_max;
+					var py_string = " 0.76 * " + px + " + 0.05 ";
+					var Hy_string_precalc = "-1 * ( " +
+					                        py_string +
+					                        " * log2(" +
+					                        py_string +
+					                        ") + (1 - " +
+					                        py_string +
+					                        ") * log2(1 - " +
+					                        py_string +
+					                        "))";
+					var Hy_string_calc = "-1 * ( " +
+					                     py +
+					                     " * log2(" +
+					                     py +
+					                     ") + (1 - " +
+					                     py +
+					                     ") * log2(1 - " +
+					                     py +
+					                     ")";
+					var HYifX_string_precalc = px + "*" + HYx0_string + " + (1 - " + px + ") * " + HYx1_string;
+					var HYifX_string_calc = px + "*" + HYx0 + " + (1 - " + px + ") * " + HYx1;
+					res_str_preprecalc = "\nпочаток \t"+ Hy_string_precalc + " - " + HYifX_string_precalc;
+					res_str_precalc ="\nпочаток2 \t"+ Hy_string_calc + " - " + HYifX_string_calc;
+					res_str_calc = "\nпочаток3 \t"+ Hy + " - " + HYifX;
+				}
+
+				px += step;
+			}
+
+			var res = v0 * max;
+			var final_str = res_str_preprecalc +
+			                " ) = " +
+			                v0 +
+			                " * (" +
+			                res_str_precalc +
+			                ") = " +
+			                v0 +
+			                " * (" +
+			                res_str_calc +
+			                " ) = " +
+			                v0 +
+			                " * " +
+			                max +
+			                " = " +
+			                res;
+			Console.WriteLine("C = " + v0 + " * max(H(Y) - H(Y|X)) = " + v0 + " * ( " + final_str);
+
+		}
+
 		internal static double Tau(int v0)
 		{
 			return 1 / (v0 * 1.0);
 		}
-		
+
 		public static double V(double v0, double IXY)
 		{
 			var val = v0 * IXY;
 			Console.WriteLine("V = " + v0 + " * " + IXY + " = " + val);
 			return val;
 		}
-		
+
 		public static void AnsverTable(string[] variables, double[] values)
 		{
 			if (variables.Length != values.Length)
 			{
 				throw new InvalidDataException("Arrays not the same length");
 			}
+
 			foreach (var _var in variables)
 			{
-				Console.Write("|"+_var+"\t");	
+				Console.Write("|" + _var + "\t");
 			}
-			Console.Write("\n");	
+
+			Console.Write("\n");
 			foreach (var _var in values)
 			{
-				Console.Write("|"+Math.Round(_var, 3)+"\t");	
+				Console.Write("|" + Math.Round(_var, 3) + "\t");
 			}
 
 
